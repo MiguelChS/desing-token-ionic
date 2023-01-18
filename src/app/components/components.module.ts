d025723c3128d4555a6d4d76bf18da3cc20e6516
miguel.chauca <miguel.chauca@y.uno>
2023-01-18 16:57:48 -0300

    feat: config sass variable maps

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { In2TextComponent } from './in2-text/in2-text.component';
+import { IonicModule } from '@ionic/angular';
+
+
+@NgModule({
+  declarations: [
+    In2TextComponent
+  ],
+  imports: [
+    IonicModule
+  ],
+  exports: [
+    In2TextComponent
+  ]
+})
+export class ComponentsModule { }
---
+++ src/app/components/in2-text/in2-text.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'in2-text',
+  templateUrl: './in2-text.component.html',
+  styleUrls: [
+    './in2-text.component.scss'
+  ]
+})
+export class In2TextComponent { }
